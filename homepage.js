9deef0e6d1990592a4abbc489f05d39a78ace424
rahul raman <rahulraman707073@gmail.com>
2023-01-21 07:52:59 +0530

    sdfds

--- homepage.js
+++ homepage.js
@@ -1,13 +1,1 @@
 
-fetchdata();
-
-
-async function fetchdata(){
-    try {
-        let res = await fetch("./product.json");
-        let data = await res.json();
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
-}
